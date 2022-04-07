5f186e2baa14ae234e7e3e4573070ce758cbe737
chz <chzphoenix@163.com>
2022-04-07 16:19:02 +0800

    modify zoom video

--- app/src/main/java/com/huichongzi/vrardemo/ZoomVideo.kt
+++ app/src/main/java/com/huichongzi/vrardemo/ZoomVideo.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.marginStart
+import androidx.core.view.marginTop
 import com.huichongzi.vrardemo.databinding.ActivityZoomVideoBinding
 import kotlin.math.max
 import kotlin.math.min
@@ -46,12 +48,12 @@ class ZoomVideo: AppCompatActivity() {
             velocityX: Float,
             velocityY: Float
         ): Boolean {
-            if(e2.y - e1.y > 10){
-                binding.videoView.seekTo(min(binding.videoView.duration, binding.videoView.currentPosition + ((e2.y - e1.y) * 100).toInt()))
+            if(e2.x - e1.x > 10){
+                binding.videoView.seekTo(min(binding.videoView.duration, binding.videoView.currentPosition + ((e2.x - e1.x) * 100).toInt()))
                 Log.e("vrvideo seek", binding.videoView.currentPosition.toString())
             }
-            else if(e2.y - e1.y < -10){
-                binding.videoView.seekTo(max(0, binding.videoView.currentPosition + ((e2.y - e1.y) * 100).toInt()))
+            else if(e2.x - e1.x < -10){
+                binding.videoView.seekTo(max(0, binding.videoView.currentPosition + ((e2.x - e1.x) * 100).toInt()))
                 Log.e("vrvideo seek", binding.videoView.currentPosition.toString())
             }
             return false
@@ -61,14 +63,23 @@ class ZoomVideo: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         binding = ActivityZoomVideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.minus.setOnClickListener {
-            resizeVideo(min(binding.root.width / 4, binding.videoView.marginStart + 60))
+        binding.minusWidth.setOnClickListener {
+            resizeVideoWidth(min(binding.root.width / 4, binding.videoView.marginStart + 30))
         }
-        binding.plus.setOnClickListener {
-            resizeVideo(max(0, binding.videoView.marginStart - 60))
+        binding.plusWidth.setOnClickListener {
+            resizeVideoWidth(max(0, binding.videoView.marginStart - 30))
+        }
+
+        binding.minusHeight.setOnClickListener {
+            resizeVideoHeight(min(binding.root.height / 4, binding.videoView.marginTop + 20))
+        }
+        binding.plusHeight.setOnClickListener {
+            resizeVideoHeight(max(0, binding.videoView.marginTop - 20))
         }
 
         val uri = intent.getParcelableExtra<Uri>("uri")
@@ -82,10 +93,17 @@ class ZoomVideo: AppCompatActivity() {
         }
     }
 
-    private fun resizeVideo(margin : Int){
+    private fun resizeVideoWidth(margin : Int){
         val layoutParams = binding.videoView.layoutParams as ConstraintLayout.LayoutParams
         layoutParams.marginEnd = margin
         layoutParams.marginStart = margin
+        binding.videoView.layoutParams = layoutParams
+    }
+
+    private fun resizeVideoHeight(margin : Int){
+        val layoutParams = binding.videoView.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.topMargin = margin
+        layoutParams.bottomMargin = margin
         binding.videoView.layoutParams = layoutParams
     }
 }
