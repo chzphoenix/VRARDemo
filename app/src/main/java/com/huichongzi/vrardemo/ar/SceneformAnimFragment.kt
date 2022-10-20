9d525371417fa5fb9d82b69b3dabb09fe391a91e
cuihz <cuihz@knowbox.cn>
2022-10-20 17:59:23 +0800

    add hat

--- app/src/main/java/com/huichongzi/vrardemo/ar/SceneformAnimFragment.kt
+++ app/src/main/java/com/huichongzi/vrardemo/ar/SceneformAnimFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.widget.TextView
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.SkeletonNode
 import com.google.ar.sceneform.animation.ModelAnimator
+import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.*
 import com.google.ar.sceneform.ux.ArFragment
@@ -18,6 +20,7 @@ import com.huichongzi.vrardemo.R
 class SceneformAnimFragment : ArFragment() {
     lateinit var bodyRenderable : ModelRenderable
     lateinit var modelAnimator : ModelAnimator
+    lateinit var hatRenderable : ModelRenderable
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,15 +33,35 @@ class SceneformAnimFragment : ArFragment() {
             modelAnimator = ModelAnimator(animData, bodyRenderable)
         }
 
+        ModelRenderable.builder().setSource(activity, R.raw.baseball_cap).build().thenAccept{
+                renderable ->
+            hatRenderable = renderable
+        }
+
 
         setOnTapArPlaneListener { hitResult, plane, motionEvent ->
             val anchor = hitResult.createAnchor()
             val anchorNode = AnchorNode(anchor)
             anchorNode.setParent(arSceneView.scene)
 
-            val transformableNode = TransformableNode(transformationSystem)
-            transformableNode.setParent(anchorNode)
-            transformableNode.renderable = bodyRenderable
+            val bodyNode = SkeletonNode()
+            bodyNode.setParent(anchorNode)
+            bodyNode.renderable = bodyRenderable
+
+            val boneNode = Node()
+            boneNode.setParent(bodyNode)
+            bodyNode.setBoneAttachment("hat_point", boneNode)
+
+            val hatNode = Node()
+            hatNode.renderable = hatRenderable
+            hatNode.setParent(boneNode)
+            hatNode.worldScale = Vector3.one()
+            hatNode.worldRotation = Quaternion.identity()
+
+            val pos = hatNode.worldPosition
+            // Lower the hat down over the antennae.
+            pos.y -= .1f
+            hatNode.worldPosition = pos
 
             modelAnimator.start()
         }
